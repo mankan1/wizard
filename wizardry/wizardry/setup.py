e7f0970a7b98abc1ba3902b35fc4613c23f8bbf7
Manoj Kandlikar <manoj.kandlikar@gmail.com>
2021-10-01 09:24:56 -0700

    added setup

---
+++ wizardry/wizardry/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wizardry',
+    version='0.0.1',
+    description='Manoj repo',
+    url='https://github.com/mankan1/wizard.git',
+    author='Raphael Schubert',
+    author_email='raphael.schubert@digitalbankscorp.com',
+    license='unlicense',
+    packages=['wizardry'],
+    zip_safe=False
+)
